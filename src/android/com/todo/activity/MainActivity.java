bf2c7ce3bc513f61cfc0790b2f58c4d1a62b94e2
曹鑫 <jingxin191314@foxmail.com>
2014-04-15 16:49:23 +0800

    add button

--- src/android/com/todo/activity/MainActivity.java
+++ src/android/com/todo/activity/MainActivity.java
@@ -2,9 +2,15 @@ package android.com.todo.activity;
 
 import android.app.Activity;
 import android.com.todo.R;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    private Button submit;
 
     /**
      * Called when the activity is first created.
@@ -15,5 +21,17 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.main);
+        submit = (Button) this.findViewById(R.id.btn_id);
+        submit.setOnClickListener(new ButtonListener());
+    }
+
+    private class ButtonListener implements OnClickListener {
+
+        public void onClick(View view) {
+            Intent intent = new Intent();
+            intent.putExtra("to", "go here!");
+            intent.setClass(MainActivity.this, ScheduleActivity.class);
+            MainActivity.this.startActivity(intent);
+        }
     }
 }
